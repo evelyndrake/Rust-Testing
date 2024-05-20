1d65395f7b5e963fed0cccf81f83f1f00f9b1dfc
Evelyn Drake <evelyndrake90@gmail.com>
2024-05-19 22:31:44 -0400

    Testing ownership changes

--- inputtesting/src/main.rs
+++ inputtesting/src/main.rs
@@ -12,6 +12,14 @@ impl Person { // Implement methods for the struct
     }
 }
 
+fn double_age(person: &mut Person) { // Function to double the age of a person
+    person.age *= 2;
+}
+
+fn remove_person(person: Person) { // Function to remove a person
+    print!("{} has been removed.", person.name);
+}
+
 fn main() {
     // Prompt the user for their information 
     println!("Enter your name:");
@@ -28,10 +36,14 @@ fn main() {
     println!("Enter your favorite color:");
     let user_fav_color: String = read!();
     // Create a new Person with the user's data
-    let user = Person {
+    let mut user = Person {
         name: user_name,
         age: user_age.parse().unwrap(),
         fav_color: user_fav_color,
     };
+    double_age(&mut user); // Call the double_age function on the user (mutably borrowed)
     user.print_greeting(); // Call the print_greeting method on the user
+    remove_person(user); // Call the remove_person function with the user as an argument
+    // The user struct is moved into the remove_person function and is no longer accessible
+    // user.print_greeting(); // This line will cause an error since user is no longer available
 }
